5fe3c83abdb0e1b1043d7b2077e2fcb8b729a435
Crypt06545 <mehedihasansagor2005@gmail.com>
2026-04-10 09:32:09 +0600

    docs: optimize site SEO metadata and implement ADA accessibility landmarks

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,31 @@
 import Background from "./components/Background";
-
 import HeroSection from "./components/Hero";
-
 import ContactSection from "./components/ContactSection";
 import Footer from "./components/Footer";
 import AboutSection from "./components/AboutSection";
 import { TechSkillsGrid } from "./components/ReviewCard";
-
 import Navbar from "./components/Navbar";
 import ProjectsSection from "./components/ProjectsSection";
 
-
 function App() {
   return (
-    <>
-      <Background>
-        <Navbar />
+    <Background>
+      <Navbar />
+
+      {/* 1. Use <main> instead of a <div> for SEO/A11y
+          2. Add id="main-content" so the skip link works
+          3. tabIndex="-1" ensures focus moves correctly
+      */}
+      <main id="main-content" tabIndex="-1" className="outline-none">
         <HeroSection />
         <AboutSection />
         <TechSkillsGrid />
-        {/* <EnvironmentSection/> */}
         <ProjectsSection />
         <ContactSection />
-        <Footer />
-      </Background>
-    </>
+      </main>
+
+      <Footer />
+    </Background>
   );
 }
 
